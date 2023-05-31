639e3f40bf0833620685587b7a1691a0e2a9bae6
Jupiter83 <superdev83@outlook.com>
2023-05-31 21:51:35 +0200

    start googleform

---
+++ src/component/GoogleForm.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import { read, utils } from 'xlsx'
+
+const GoogleForm = () => {
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <>
+      <h1>GoogleForm</h1>
+    </>
+  );
+}
+
+
+export default GoogleForm;
